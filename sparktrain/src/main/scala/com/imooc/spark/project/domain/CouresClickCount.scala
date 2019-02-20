7c84d43f80c2df2cdc71ee08242d840f7adebeab
Aayers-ghw <1515182041@qq.com>
2019-02-20 18:19:15 +0800

    2019/2/20

--- sparktrain/src/main/scala/com/imooc/spark/project/domain/CouresClickCount.scala
+++
@@ -1,8 +0,0 @@
-package com.imooc.spark.project.domain
-
-/**
-  * 实战课程点击数实体类
-  * @param day_course 对应的就是HBase中的rowkey，20181222-1
-  * @param click_count  对应的20181222_1的访问总数
-  */
-case class CourseClickCount (day_course:String, click_count:Long)
